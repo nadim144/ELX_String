91dcf6861af1bac20caeedd15ad4ea097523eb08
Md Nadim Ahmad <ahmad.nadim144@gmail.com>
2023-03-19 00:41:16 +0530

    ELX_String first commit

---
+++ ELX_String/ELX_String.h
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////
+//  Copyright Md Nadim Ahmad Suopaul, BIHAR, INDIA 2023.         //
+//  ELX_Sting implementation                                     //
+//                                                               //
+//                                                               //
+///////////////////////////////////////////////////////////////////
+
+
+#pragma once
+class ELX_String
+{
+};
+
---
+++ ELX_String/ELX_StringTest.cpp
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////
+//  Copyright Md Nadim Ahmad Suopaul, BIHAR, INDIA 2023.         //
+//  ELX_Sting Test Example                                       //
+//                                                               //
+//                                                               //
+///////////////////////////////////////////////////////////////////
+
+// ELX_String.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
